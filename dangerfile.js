2d97a776264aee5ca4f495784a5ccc460d7525ed
AndreaPontrandolfo <andrea.pontra@gmail.com>
2022-10-23 18:44:47 +0200

    feat: async trick

--- dangerfile.js
+++ dangerfile.js
@@ -1,6 +1,7 @@
 import { message, danger, warn } from "danger";
 
 const modifiedMD = danger.git.modified_files.join("- ");
+
 message("Second PR PR! Congrats!");
 message("Changed Files in this PR: \n - " + modifiedMD);
-warn("## 🤔" + danger.git.linesOfCode());
+warn("## 🤔" + (async () => await danger.git.linesOfCode())());
