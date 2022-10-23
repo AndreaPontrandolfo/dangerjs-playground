27da00984795042e5cfa844148b36ab707839d64
AndreaPontrandolfo <andrea.pontra@gmail.com>
2022-10-23 20:36:08 +0200

    feat added fn warnForNewEslintDisableDirectivesFound

--- dangerfile.js
+++ dangerfile.js
@@ -1,22 +1,26 @@
-import { message, danger, warn } from "danger";
+import { danger, warn } from "danger";
 
 const modifiedFiles = danger.git.modified_files.filter(
   (path) => path.endsWith("js") || path.endsWith("ts")
 );
 
-const structuredModifiedFiles = modifiedFiles.map((file) => {
-  return {
-    fileName: file,
-    fileDiffPromise: danger.git.diffForFile(file),
-  };
-});
-
-for (const structuredModifiedFile of structuredModifiedFiles) {
-  structuredModifiedFile.fileDiffPromise.then((fileDiff) => {
-    if (fileDiff.added.includes("eslint-disable")) {
-      warn(
-        `### 🤔 An "eslint-disable" was added in file: ${structuredModifiedFile.fileName}`
-      );
-    }
+const warnForNewEslintDisableDirectivesFound = () => {
+  const structuredModifiedFiles = modifiedFiles.map((file) => {
+    return {
+      fileName: file,
+      fileDiffPromise: danger.git.diffForFile(file),
+    };
   });
-}
+
+  for (const structuredModifiedFile of structuredModifiedFiles) {
+    structuredModifiedFile.fileDiffPromise.then((fileDiff) => {
+      if (fileDiff.added.includes("eslint-disable")) {
+        warn(
+          `🤔 An "eslint-disable" directive was added in file: "${structuredModifiedFile.fileName}"`
+        );
+      }
+    });
+  }
+};
+
+warnForNewEslintDisableDirectivesFound();
