782b1090a712d84ca768f1ee68a443c369e971a9
AndreaPontrandolfo <andrea.pontra@gmail.com>
2022-10-23 19:23:41 +0200

    difss3

--- dangerfile.js
+++ dangerfile.js
@@ -9,8 +9,19 @@ const files = danger.git.modified_files.filter(
   (path) => path.endsWith("js") || path.endsWith("ts")
 );
 
-const diffsPromises = files.map((file) => danger.git.JSONDiffForFile(file));
-
-Promise.all(diffsPromises).then((JSONDiffs) => {
-  warn("## 🤔" + JSON.stringify(JSONDiffs[0]));
+files.forEach((file) => {
+  const fileContents = fs.readFileSync(file).toString();
+  console.log(
+    "🚀 ~ file: dangerfile.js ~ line 14 ~ files.forEach ~ content",
+    fileContents
+  );
+  if (fileContents.includes("eslint-disable")) {
+    warn("## 🤔 Found a eslint disable!");
+  }
 });
+
+// const diffsPromises = files.map((file) => danger.git.JSONDiffForFile(file));
+
+// Promise.all(diffsPromises).then((JSONDiffs) => {
+//   warn("## 🤔" + JSON.stringify(JSONDiffs[0]));
+// });
