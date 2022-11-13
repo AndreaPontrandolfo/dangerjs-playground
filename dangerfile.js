d5224fb24a2d6bde2cde92c3e58054df16ce0ae1
AndreaPontrandolfo <andrea.pontra@gmail.com>
2022-11-13 20:39:24 +0100

    build(deps): upgraded danger-plugin-eslint-disable-warn

--- dangerfile.js
+++ dangerfile.js
@@ -1,7 +1,3 @@
 const eslintDisableWarn = require("danger-plugin-eslint-disable-warn").default;
-console.log(
-  "🚀 ~ file: dangerfile.js ~ line 3 ~ eslintDisableWarn",
-  eslintDisableWarn
-);
 
 eslintDisableWarn();
